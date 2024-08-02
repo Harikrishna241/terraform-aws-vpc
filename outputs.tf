ff19d75f2f9b9f84be2c0c6748e3b5d01af3b2bf
Harikrishna241 <hkaranam241@gmail.com>
2024-08-02 05:12:40 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "vpc_id" {
     value = aws_vpc.expense.id
   
 }
+
+output "public_subnet_id" {
+    value = aws_subnet.public[*].id
+  
+}
+
+output "privte_subnet_id" {
+    value = aws_subnet.Backend[*].id
+  
+}
+
+output "db_subnet_id" {
+    value = aws_subnet.db[*].id
+  
+}
